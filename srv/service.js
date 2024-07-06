d3afbd9b7dd85dd6d351a993d65ce044186705d3
Milton Chandradas <milton.chandradas@sap.com>
2024-07-06 03:33:02 -0400

    customers...

--- srv/service.js
+++ srv/service.js
@@ -1,128 +1,147 @@
 const cds = require("@sap/cds");
 
 module.exports = async (srv) => {
-    const { MappingCustomers, Customers, S4SalesOrders, NorthwindCustomers } = srv.entities;
+  const { MappingCustomers, Customers, S4SalesOrders, NorthwindCustomers } =
+    srv.entities;
 
-    // connect to S/4HANA
-    const S4_Service = await cds.connect.to("SalesOrderA2X");
+  // connect to S/4HANA
+  const S4_Service = await cds.connect.to("SalesOrderA2X");
 
-    // connect to Northwind
-    const Northwind_Service = await cds.connect.to("northwind");
+  // connect to Northwind
+  const Northwind_Service = await cds.connect.to("northwind");
 
-    srv.on("READ", Customers, async (req) => {
+  const delay = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
 
-        const getCustomers = async () => {
+  srv.on("READ", Customers, async (req) => {
+    // Sequential...
+    // await delay(10000)
+    // await delay(10000)
+    // return [];
 
-            if (req.data?.customerId) {
-                let customer = await Northwind_Service.send({
-                    query: SELECT.one.from(NorthwindCustomers).where({ customerId: req.data?.customerId }),
-                });
+    // Parallel...
+    // await Promise.all([delay(10000), delay(10000)])
+    // return [];
 
-                customer = await getS4CustomerId(customer)
-                customer = await getS4Orders(customer)
-                return customer
-            }
-            let customers = await Northwind_Service.send({
-                query: SELECT.from(NorthwindCustomers),
-            });
+    const mappings = await SELECT.from(MappingCustomers);
 
-            customers.$count = customers.length;
-            return customers;
-        }
+    const getCustomers = async () => {
+      let customers = await Northwind_Service.send({
+        query: SELECT.from(NorthwindCustomers),
+      });
 
-        const getS4CustomerId = async (customer) => {
-            let mapping = await SELECT.one.from(MappingCustomers).where({
-                nwCustomerId: customer.customerId
-            })
+      customers.$count = customers.length;
+      return customers;
+    };
 
-            customer.s4CustomerId = mapping.s4CustomerId;
-            return customer;
-        }
+    const getCustomer = async () => {
+      let customer = await Northwind_Service.send({
+        query: SELECT.one
+          .from(NorthwindCustomers)
+          .where({ customerId: req.data?.customerId }),
+      });
 
-        const getS4Orders = async (customer) => {
-            const orders = await S4_Service.send({
-                query: SELECT.from(S4SalesOrders)
-                    .where({ customerId: customer.s4CustomerId })
-                    .columns("salesOrder", "customerId", "salesOrderDate", "totalAmount", "status")
-                    .limit(10),
-                headers: {
-                    apikey: process.env.apikey,
-                },
-            });
-            customer.orders = orders;
-            customer.orders.$count = orders.length
-            return customer
-        }
+      customer = await getS4CustomerId(customer);
+      customer = await getS4Orders(customer);
+      return customer;
+    };
 
-        if (!req.query.SELECT.columns) return getCustomers();
+    const getS4CustomerId = async (customer) => {
+      const mapping = mappings.find(
+        (mapping) => mapping.nwCustomerId === customer.customerId
+      );
+      customer.s4CustomerId = mapping?.s4CustomerId;
+      return customer;
+    };
 
-        const expandIndex = req.query.SELECT.columns.findIndex(
-            ({ expand, ref }) => expand && ref[0] === "orders"
-        );
-        console.log(req.query.SELECT.columns);
-        if (expandIndex < 0) return getCustomers();
+    const getS4Orders = async (customer) => {
+      const orders = await S4_Service.send({
+        query: SELECT.from(S4SalesOrders)
+          .where({ customerId: customer.s4CustomerId })
+          .columns(
+            "salesOrder",
+            "customerId",
+            "salesOrderDate",
+            "totalAmount",
+            "status"
+          )
+          .limit(10),
+        headers: {
+          apikey: process.env.apikey,
+        },
+      });
+      customer.orders = orders;
+      customer.orders.$count = orders.length;
+      return customer;
+    };
 
-        req.query.SELECT.columns.splice(expandIndex, 1);
-        if (
-            !req.query.SELECT.columns.find((column) =>
-                column.ref.find((ref) => ref == "orders")
-            )
-        ) {
-            req.query.SELECT.columns.push({ ref: ["orders_salesOrder"] });
-        }
+    if (!req.data?.customerId) return getCustomers();
 
+    let customer = await getCustomer();
+    customer = await getS4Orders(customer);
 
-        let customers = await Northwind_Service.send({
-            query: SELECT.from(NorthwindCustomers).limit(5)
-        })
+    return customer;
+  });
 
-        await Promise.all(
-            customers.map(async (customer) => {
-                await getS4CustomerId(customer)
-            })
-        )
+  srv.on("READ", S4SalesOrders, async (req) => {
+    let customerId = req.params[0]?.customerId;
+    let orders;
 
-        try {
-            customers = Array.isArray(customers) ? customers : [customers];
+    if (!customerId) {
+      orders = await S4_Service.send({
+        query: SELECT.from(S4SalesOrders)
+          .columns(
+            "salesOrder",
+            "customerId",
+            "salesOrderDate",
+            "totalAmount",
+            "status"
+          )
+          .limit(10),
+        headers: {
+          apikey: process.env.apikey,
+          Accept: "application/json",
+        },
+      });
+    } else {
+      const mapping = await SELECT.one
+        .from(MappingCustomers)
+        .where({ nwCustomerId: customerId });
 
-            return await Promise.all(
-                customers.map(async (customer) => {
-                    customer = await getS4Orders(customer)
-                    return customer
-                })
-            );
-        } catch (error) {
-            console.log("Error: ", error)
-        }
-    })
+      orders = await S4_Service.send({
+        query: SELECT.from(S4SalesOrders)
+          .columns(
+            "salesOrder",
+            "customerId",
+            "salesOrderDate",
+            "totalAmount",
+            "status"
+          )
+          .where({ customerId: mapping.s4CustomerId })
+          .limit(10),
+        headers: {
+          apikey: process.env.apikey,
+          Accept: "application/json",
+        },
+      });
+    }
 
-    srv.on("READ", S4SalesOrders, async (req) => {
-        let orders = await S4_Service.send({
-            query: SELECT.from(S4SalesOrders)
-                .columns("salesOrder", "customerId", "salesOrderDate", "totalAmount", "status")
-                .limit(10),
-            headers: {
-                apikey: process.env.apikey,
-                Accept: "application/json"
-            },
-        });
+    orders.$count = orders.length;
+    return orders;
+  });
 
-        orders.$count = orders.length
-        return orders
-    })
+  srv.after("READ", S4SalesOrders, (data) => {
+    const orders = Array.isArray(data) ? data : [data];
 
-    srv.after("READ", S4SalesOrders, (data) => {
-        const orders = Array.isArray(data) ? data : [data];
-
-        orders.forEach(order => {
-            if (order.status === 'C') {
-                order.status = "Completed"
-                order.criticality = 3
-            } else {
-                order.status = "Over Due"
-                order.criticality = 1
-            }
-        })
-    })
-
-}
+    orders.forEach((order) => {
+      if (order.status === "C") {
+        order.status = "Completed";
+        order.criticality = 3;
+      } else {
+        order.status = "Over Due";
+        order.criticality = 1;
+      }
+    });
+  });
+};
