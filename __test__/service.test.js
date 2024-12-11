04685969581c04707bc841550558938524c1dc41
Milton Chandradas <milton.chandradas@sap.com>
2024-12-11 11:21:46 -0500

    initial commit...

--- __test__/service.test.js
+++ __test__/service.test.js
@@ -1,6 +1,19 @@
+const myService = require("../srv/service");
+const cds = require("@sap/cds");
+
 describe("CAP Tests - Group 1", () => {
+  const { GET } = cds.test(
+    "serve",
+    __dirname + "/../srv"
+  );
+
   it("test 1", () => {
     const { name } = { name: "Milton" };
     expect(name).toBe("Milton");
+  });
+
+  it("test 2", async () => {
+    const {status} = await GET("/service/fromABAPtoCAPSvcs/Customers")
+    expect(status).toBe(200)
   });
 });
